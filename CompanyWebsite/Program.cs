432cabda1201d983972cd44d7e6bda7aee69e82e
AlexanderLukashuk <upiter465@gmail.com>
2023-06-19 02:28:23 +0600

    Added new configurations in Program class

--- CompanyWebsite/Program.cs
+++ CompanyWebsite/Program.cs
@@ -1,8 +1,38 @@
 ﻿using CompanyWebsite.Service;
+using CompanyWebsiteLibrary.Data;
+using CompanyWebsiteLibrary.Data.Repositories.Abstract;
+using CompanyWebsiteLibrary.Data.Repositories.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.Bind("Project", new Config());
+
+builder.Services.AddTransient<ITextFieldsRepository, EFTextFieldRepository>();
+builder.Services.AddTransient<IServiceItemsREpository, EFServiceItemsRepository>();
+builder.Services.AddTransient<DataManager>();
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+{
+    opts.User.RequireUniqueEmail = true;
+    opts.Password.RequiredLength = 6;
+    opts.Password.RequireNonAlphanumeric = false;
+    opts.Password.RequireLowercase = false;
+    opts.Password.RequireUppercase = false;
+    opts.Password.RequireDigit = false;
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.Name = "myCompanyAuth";
+    options.Cookie.HttpOnly = true;
+    options.LoginPath = "/account/login";
+    options.AccessDeniedPath = "/account/accessdenied";
+    options.SlidingExpiration = true;
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,6 +52,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
