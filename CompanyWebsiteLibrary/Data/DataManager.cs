be0678dee609ae4c435eed5e17d4f676a43a8e58
AlexanderLukashuk <upiter465@gmail.com>
2023-06-19 02:13:39 +0600

    Created DataManager class to work with repositories

---
+++ CompanyWebsiteLibrary/Data/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CompanyWebsiteLibrary.Data.Repositories.Abstract;
+
+namespace CompanyWebsiteLibrary.Data
+{
+	public class DataManager
+	{
+		public ITextFieldsRepository? TextFields { get; set; }
+		public IServiceItemsREpository? ServiceItems { get; set; }
+
+		public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsREpository serviceItemsREpository)
+		{
+			TextFields = textFieldsRepository;
+			ServiceItems = serviceItemsREpository;
+		}
+	}
+}
+
