658170b1d9d6b97c4d1eb1c45df72cfcd3c2c4ed
AlexanderLukashuk <upiter465@gmail.com>
2023-06-19 01:54:17 +0600

    Created Repository(Interface) for ServiceItem

---
+++ CompanyWebsiteLibrary/Data/Repositories/Abstract/IServiceItemsREpository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CompanyWebsiteLibrary.Entities;
+
+namespace CompanyWebsiteLibrary.Data.Repositories.Abstract
+{
+	public interface IServiceItemsREpository
+	{
+		IQueryable<ServiceItem> GetServiceItems();
+		ServiceItem GetServiceItemById(Guid id);
+		void SaveServiceItem(ServiceItem entity);
+		void DeleteServiceItem(Guid id);
+	}
+}
+
