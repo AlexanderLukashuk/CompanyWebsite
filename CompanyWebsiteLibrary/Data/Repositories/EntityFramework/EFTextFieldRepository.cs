aff9bae386c02fdf9c5fd1b70d4998d5cd5f715d
AlexanderLukashuk <upiter465@gmail.com>
2023-06-19 02:04:46 +0600

    Created EF Repository for TextField

---
+++ CompanyWebsiteLibrary/Data/Repositories/EntityFramework/EFTextFieldRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CompanyWebsiteLibrary.Data.Repositories.Abstract;
+using CompanyWebsiteLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyWebsiteLibrary.Data.Repositories.EntityFramework
+{
+	public class EFTextFieldRepository : ITextFieldsRepository
+    {
+		private readonly AppDbContext context;
+
+		public EFTextFieldRepository(AppDbContext ctx)
+		{
+			context = ctx;
+		}
+
+        public IQueryable<TextField> GetTextFields()
+        {
+            return context.TextFields;
+        }
+
+        public TextField GetTextFieldById(Guid id)
+        {
+            return context.TextFields.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TextField GetTextFieldByCodeWord(string codeWord)
+        {
+            return context.TextFields.FirstOrDefault(x => x.CodeWord == codeWord);
+        }
+
+        public void SaveTextField(TextField entity)
+        {
+            if (entity.Id == default)
+            {
+                context.Entry(entity).State = EntityState.Added;
+            }
+            else
+            {
+                context.Entry(entity).State = EntityState.Modified;
+            }
+            context.SaveChanges();
+        }
+
+        public void DeleteTextField(Guid id)
+        {
+            context.TextFields.Remove(new TextField() { Id = id });
+            context.SaveChanges();
+        }
+    }
+}
+
