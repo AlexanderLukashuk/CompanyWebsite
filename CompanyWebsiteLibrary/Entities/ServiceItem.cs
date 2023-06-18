67156fc9a9a8355990531d3ecc87cb553a936421
AlexanderLukashuk <upiter465@gmail.com>
2023-06-18 22:02:32 +0600

    Created TextField and ServiceItem classes

---
+++ CompanyWebsiteLibrary/Entities/ServiceItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyWebsiteLibrary.Entities
+{
+	public class ServiceItem : EntityBase
+	{
+		[Required(ErrorMessage = "Fill in the name of the service")]
+		[Display(Name = "Service name")]
+		public override string? Title { get; set; } = "Information page";
+
+        [Display(Name = "Full description")]
+        public override string? Text { get; set; }
+    }
+}
+
---
+++ CompanyWebsiteLibrary/Entities/TextField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyWebsiteLibrary.Entities
+{
+	public class TextField : EntityBase
+	{
+		[Required]
+		public string? CodeWord { get; set; }
+
+		[Display(Name = "Page Name")]
+		public override string? Title { get; set; } = "Information page";
+
+        [Display(Name = "Page content")]
+        public override string? Text { get; set; } = "Content is filled by the administrator";
+    }
+}
+
