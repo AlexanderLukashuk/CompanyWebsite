808560b9f687f92b778def8a875286d8b2a19fe0
AlexanderLukashuk <upiter465@gmail.com>
2023-06-24 08:55:37 +0600

    Created ViewModel for login

---
+++ CompanyWebsite/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyWebsite.Models
+{
+	public class LoginViewModel
+	{
+		[Required]
+		[Display(Name = "Login")]
+		public string? UserName { get; set; }
+
+        [Required]
+		[UIHint("password")]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Display(Name = "Remember the password?")]
+        public bool RememberPassword { get; set; }
+    }
+}
+
