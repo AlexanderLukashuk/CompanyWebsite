8875cd66eac1d7b88f212e0f99c898c0c34c0179
AlexanderLukashuk <upiter465@gmail.com>
2023-06-26 14:33:51 +0600

    Created class Extensions with static method CutController

---
+++ CompanyWebsite/Service/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CompanyWebsite.Service
+{
+	public static class Extensions
+	{
+		public static string CutController(this string str)
+		{
+			return str.Replace("Controller", "");
+		}
+	}
+}
+
