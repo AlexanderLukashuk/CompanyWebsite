d5483e4f39ad43ac2e900021978589f031548378
AlexanderLukashuk <upiter465@gmail.com>
2023-06-26 14:31:37 +0600

    Created TextFieldsController

---
+++ CompanyWebsite/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyWebsiteLibrary.Data;
+using CompanyWebsiteLibrary.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompanyWebsite.Areas.Admin.Controllers
+{
+    public class TextFieldsController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public TextFieldsController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Edit(string codeWord)
+        {
+            var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TextField model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.TextFields.SaveTextField(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+    }
+}
+
