8f20480fe24267fd5bb16cdc9b8089695fe45679
AlexanderLukashuk <upiter465@gmail.com>
2023-06-26 21:37:42 +0600

    Added Attribute for Admin area in TextFieldsContorller

--- CompanyWebsite/Areas/Admin/Controllers/TextFieldsController.cs
+++ CompanyWebsite/Areas/Admin/Controllers/TextFieldsController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyWebsite.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class TextFieldsController : Controller
     {
         private readonly DataManager dataManager;
