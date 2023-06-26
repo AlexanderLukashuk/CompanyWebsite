47e1d2eaa2eadcb0972a18093f5244195f28d558
AlexanderLukashuk <upiter465@gmail.com>
2023-06-26 13:52:24 +0600

    Added DataManager in Admin/HomeController

--- CompanyWebsite/Areas/Admin/Controllers/HomeController.cs
+++ CompanyWebsite/Areas/Admin/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CompanyWebsiteLibrary.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +12,16 @@ namespace CompanyWebsite.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItems());
         }
     }
 }
