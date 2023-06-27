4f6ab2d0f86b9e009d4b9fc2fb72b1ccfe771e0f
AlexanderLukashuk <upiter465@gmail.com>
2023-06-27 09:21:08 +0600

    Created ServiceItemsController

---
+++ CompanyWebsite/Areas/Admin/Controllers/ServiceItemsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyWebsite.Service;
+using CompanyWebsiteLibrary.Data;
+using CompanyWebsiteLibrary.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompanyWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceItemsController : Controller
+    {
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public ServiceItemsController(DataManager dataManager, IWebHostEnvironment hostEnvironment)
+        {
+            this.dataManager = dataManager;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var entity = id == default ? new ServiceItem() : dataManager.ServiceItems.GetServiceItemById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ServiceItem model, IFormFile titleIgameFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (titleIgameFile != null)
+                {
+                    model.TitleImagePath = titleIgameFile.FileName;
+                    using (var stream = new FileStream(Path.Combine(hostEnvironment.WebRootPath, "images/", titleIgameFile.FileName), FileMode.Create))
+                    {
+                        titleIgameFile.CopyTo(stream);
+                    }
+                }
+                dataManager.ServiceItems.SaveServiceItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.ServiceItems.DeleteServiceItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+        }
+    }
+}
+
