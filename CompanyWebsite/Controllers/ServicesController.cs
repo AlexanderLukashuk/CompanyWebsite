e70672a928d691d65786bfdd02bc1ca436044746
AlexanderLukashuk <upiter465@gmail.com>
2023-06-28 19:36:26 +0600

    Created ServicesController

---
+++ CompanyWebsite/Controllers/ServicesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CompanyWebsiteLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompanyWebsite.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public ServicesController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            if (id != default)
+            {
+                return View("Show", dataManager.ServiceItems.GetServiceItemById(id));
+            }
+
+            ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PageServices");
+            return View(dataManager.ServiceItems.GetServiceItems());
+        }
+    }
+}
+
