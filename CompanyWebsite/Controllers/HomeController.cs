8d0abbf33a82e37893c65332d23fc75afa3061da
AlexanderLukashuk <upiter465@gmail.com>
2023-06-28 18:57:44 +0600

    Changed HomeController: deleted unused methods, created DataManager entity, new constructor, created method for Contacts

--- CompanyWebsite/Controllers/HomeController.cs
+++ CompanyWebsite/Controllers/HomeController.cs
@@ -1,32 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CompanyWebsite.Models;
+using CompanyWebsiteLibrary.Data;
 
 namespace CompanyWebsite.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly DataManager dataManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(DataManager dataManager)
     {
-        _logger = logger;
+        this.dataManager = dataManager;
     }
 
     public IActionResult Index()
     {
-        return View();
+        return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
     }
 
-    public IActionResult Privacy()
+    public IActionResult Contacts()
     {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
     }
 }
 
