568ff69683c6c3226d399ff40328e98cd0b433bb
AlexanderLukashuk <upiter465@gmail.com>
2023-06-28 20:13:31 +0600

    Created Class SidebarViewComponent with method InvokeAsync

---
+++ CompanyWebsite/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CompanyWebsiteLibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyWebsite.Models
+{
+	public class SidebarViewComponent : ViewComponent
+	{
+		private readonly DataManager dataManager;
+
+		public SidebarViewComponent(DataManager dataManager)
+		{
+			this.dataManager = dataManager;
+		}
+
+	}
+}
+
